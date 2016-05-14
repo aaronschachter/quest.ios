f482be547ae84b168a463426cce01c7f510a8fd8
Aaron Schachter <aaronschachter@gmail.com>
2016-05-14 09:57:44 -0700

    Initial Commit

---
+++ InterviewQuest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InterviewQuest
+//
+//  Created by Aaron Schachter on 5/14/16.
+//  Copyright © 2016 New School Old School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InterviewQuest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterviewQuest
+//
+//  Created by Aaron Schachter on 5/14/16.
+//  Copyright © 2016 New School Old School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
