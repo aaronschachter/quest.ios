87e6d1a15478253528914d0006fa07926207a119
Aaron Schachter <aaronschachter@gmail.com>
2016-05-15 07:33:32 -0700

    Fixes bug where question is added after selecting Cancel

--- InterviewQuest/Controllers/IVQQuestionDetailViewController.m
+++ InterviewQuest/Controllers/IVQQuestionDetailViewController.m
@@ -12,6 +12,7 @@
 @interface IVQQuestionDetailViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextView *questionTitleTextView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *addQuestionBarButtonItem;
 
 @end
 
@@ -22,6 +23,9 @@
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if (sender != self.addQuestionBarButtonItem) {
+        return;
+    }
     IVQQuestionsViewController *destVC = (IVQQuestionsViewController *)segue.destinationViewController;
     [destVC addNewQuestionWithTitle:self.questionTitleTextView.text];
 }
