37d9d07205286393520bfa6c6bc14b3619d370cd
Aaron Schachter <aaronschachter@gmail.com>
2016-05-17 21:23:28 -0700

    Adds IVQQuestionsView.xib, deletes Main.storyboard

--- InterviewQuest/AppDelegate.m
+++ InterviewQuest/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "IVQQuestionsViewController.h";
 
 @interface AppDelegate ()
 
@@ -16,7 +17,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self.window makeKeyAndVisible];
+    IVQQuestionsViewController *questionsViewController = [[IVQQuestionsViewController alloc] initWithNibName:@"IVQQuestionsView" bundle:nil];
+    questionsViewController.title = @"Questions";
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:questionsViewController];
+
     return YES;
 }
 
