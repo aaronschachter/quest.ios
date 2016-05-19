74d7bec7be946759281caadd91ea6926e45c3228
Aaron Schachter <aaronschachter@gmail.com>
2016-05-18 22:08:23 -0700

    Basic loop through all questions with nextButton

--- InterviewQuest/Controllers/IVQPlayViewController.m
+++ InterviewQuest/Controllers/IVQPlayViewController.m
@@ -12,24 +12,56 @@
 
 @interface IVQPlayViewController ()
 
+@property (assign, nonatomic) BOOL gameCompleted;
+@property (strong, nonatomic) NSArray *questions;
+@property (assign, nonatomic) NSInteger currentQuestionNumber;
+
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
 @property (weak, nonatomic) IBOutlet UILabel *questionTitleLabel;
 
 - (IBAction)nextButtonTouchUpInside:(id)sender;
-@property (weak, nonatomic) IBOutlet UIButton *nextButton;
+
 @end
 
 @implementation IVQPlayViewController
+
+#pragma mark - UIViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     __block AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
-    if (appDelegate.questions.count > 0) {
-        IVQQuestion *question = (IVQQuestion *)appDelegate.questions[0];
+    self.questions = appDelegate.questions;
+    self.currentQuestionNumber = 0;
+    self.gameCompleted = NO;
+    [self loadCurrentQuestion];
+}
+
+#pragma mark - IBAction
+
+- (IBAction)nextButtonTouchUpInside:(id)sender {
+    if (self.gameCompleted) {
+        [self.navigationController popViewControllerAnimated:YES];
+        return;
+    }
+    self.currentQuestionNumber++;
+    if (self.currentQuestionNumber == self.questions.count - 1) {
+        self.questionTitleLabel.text = @"Quest completed! You earned 10 coins.";
+        [self.nextButton setTitle:@"Done" forState:UIControlStateNormal];
+        self.gameCompleted = YES;
+        return;
+    }
+    [self loadCurrentQuestion];
+    
+}
+
+#pragma mark - IVQPlayViewController
+
+- (void)loadCurrentQuestion {
+    if (self.questions.count > 0) {
+        IVQQuestion *question = (IVQQuestion *)self.questions[self.currentQuestionNumber];
         self.questionTitleLabel.text = question.title;
     }
 }
 
-- (IBAction)nextButtonTouchUpInside:(id)sender {
-}
 @end
