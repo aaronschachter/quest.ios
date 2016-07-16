e1e273545e918be4fde9e330302ce0c4b5611e5e
Aaron Schachter <aaronschachter@gmail.com>
2016-07-15 19:11:26 -0700

    Starts on Profile tableView

--- InterviewQuest/Controllers/Profile/IVQProfileViewController.m
+++ InterviewQuest/Controllers/Profile/IVQProfileViewController.m
@@ -2,11 +2,9 @@
 #import <ionicons/IonIcons.h>
 @import Firebase;
 
-@interface IVQProfileViewController ()
+@interface IVQProfileViewController () <UITableViewDataSource, UITableViewDelegate>
 
-@property (weak, nonatomic) IBOutlet UILabel *userNameLabel;
-
-- (IBAction)signoutButtonTouchUpInside:(id)sender;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -22,16 +20,46 @@
         }
     }
     UIImage *menuImage = [IonIcons imageWithIcon:ion_ios_close_empty size:22.0f color:self.view.tintColor];
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:menuImage style:UIBarButtonItemStylePlain target:self action:@selector(dismissModalViewControllerAnimated:)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithImage:menuImage style:UIBarButtonItemStylePlain target:self action:@selector(dismiss)];
+
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    NSString* cellIdentifier = @"cell";
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellIdentifier];
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    self.tableView.scrollEnabled = NO;
 }
 
-- (IBAction)signoutButtonTouchUpInside:(id)sender {
+- (void)dismiss {
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)signOut {
     NSError *error;
     [[FIRAuth auth] signOut:&error];
     if (!error) {
-        NSLog(@"signout");
-        [self dismissModalViewControllerAnimated:YES];
+        [self dismiss];
     }
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.textLabel.text = @"Sign out";
+
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self signOut];
 }
 
 @end
